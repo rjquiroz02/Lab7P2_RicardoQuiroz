55b05e450be8bbe9d8b25cc6dcfd8642e1fbb0d2
rjquiroz02 <rjquiroz02@unitec.edu>
2022-09-02 16:05:51 -0600

    Commit14

--- src/lab7p2_ricardoquiroz/Claseadmin.java
+++ src/lab7p2_ricardoquiroz/Claseadmin.java
@@ -5,6 +5,7 @@
  */
 package lab7p2_ricardoquiroz;
 
+import java.io.File;
 import java.util.ArrayList;
 
 /**
@@ -14,6 +15,19 @@ import java.util.ArrayList;
 public class Claseadmin {
     private ArrayList<Planta> lplantas = new ArrayList();
     private ArrayList<Zombie> lzombies = new ArrayList();
+    private File archivo = null;
+    
+    public Claseadmin(String path){
+        archivo = new File(path);
+    }
+    
+    public File getArchivo(){
+        return archivo;
+    }
+    
+    public void setArchivo(File archivo){
+        this.archivo=archivo;
+    }
 
     public ArrayList<Planta> getLplantas() {
         return lplantas;
@@ -36,9 +50,21 @@ public class Claseadmin {
         return "lplantas=" + lplantas + ", lzombies=" + lzombies;
     }
     
+    //ex
+    public void setPlanta(Planta p){
+        this.lplantas.add(p);
+    }
     
+    public void setZombie(Zombie z){
+        this.lzombies.add(z);
+    }
     
+    public void escribirArchivo(){
+        
+    }
     
-    
+    public void cargarArchivo(){
+        
+    }
     
 }
--- src/lab7p2_ricardoquiroz/MainPVZ.java
+++ src/lab7p2_ricardoquiroz/MainPVZ.java
@@ -20,10 +20,12 @@ public class MainPVZ extends javax.swing.JFrame {
      */
     public MainPVZ() throws FileNotFoundException {
         initComponents();
-        FileReader frp = new FileReader("./Plantas.txt");
-        BufferedReader brp = new BufferedReader(frp);
-        FileReader frz = new FileReader("./Zombies.txt");
-        BufferedReader brz = new BufferedReader(frz);
+//        FileReader frp = new FileReader("./Plantas.txt");
+//        BufferedReader brp = new BufferedReader(frp);
+//        FileReader frz = new FileReader("./Zombies.txt");
+//        BufferedReader brz = new BufferedReader(frz);
+        Claseadmin caP = new Claseadmin("./Plantas.txt");
+        Claseadmin caZ = new Claseadmin("./Zombies.txt");
         
     }
 
