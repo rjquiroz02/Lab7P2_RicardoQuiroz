fe52af6dbe38aa6ee9c93717b9d28c69e7485a20
rjquiroz02 <rjquiroz02@unitec.edu>
2022-09-02 15:48:12 -0600

    Commit13

--- src/lab7p2_ricardoquiroz/Claseadmin.java
+++ src/lab7p2_ricardoquiroz/Claseadmin.java
@@ -5,11 +5,38 @@
  */
 package lab7p2_ricardoquiroz;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author rjqer
  */
 public class Claseadmin {
+    private ArrayList<Planta> lplantas = new ArrayList();
+    private ArrayList<Zombie> lzombies = new ArrayList();
+
+    public ArrayList<Planta> getLplantas() {
+        return lplantas;
+    }
+
+    public void setLplantas(ArrayList<Planta> lplantas) {
+        this.lplantas = lplantas;
+    }
+
+    public ArrayList<Zombie> getLzombies() {
+        return lzombies;
+    }
+
+    public void setLzombies(ArrayList<Zombie> lzombies) {
+        this.lzombies = lzombies;
+    }
+
+    @Override
+    public String toString() {
+        return "lplantas=" + lplantas + ", lzombies=" + lzombies;
+    }
+    
+    
     
     
     
